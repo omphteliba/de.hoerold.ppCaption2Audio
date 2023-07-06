3b069fcefae347e0aabf4d582174308e558654e4
omphteliba <oliver@hoerold.de>
2023-07-06 21:31:30 +0200

    Stuff thet ChatGPT told me to add to the repo

--- client/index.js
+++ client/index.js
@@ -1,9 +1,87 @@
-/* Create an instance of CSInterface. */
 var csInterface = new CSInterface();
-/* Make a reference to your HTML button and add a click handler. */
-var openButton = document.querySelector("#open-button");
-openButton.addEventListener("click", openDoc);
-/* Write a helper function to pass instructions to the ExtendScript side. */
-function openDoc() {
-  csInterface.evalScript("openDocument()");
+
+function openTab(tabId) {
+    var tabs = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+    document.getElementById(tabId).style.display = 'block';
 }
+
+// Simple encryption function
+function encrypt(text) {
+    var encryptedText = window.btoa(text); // Base64 encoding
+    return encryptedText;
+}
+
+// Simple decryption function
+function decrypt(encryptedText) {
+    var text = window.atob(encryptedText); // Base64 decoding
+    return text;
+}
+
+function saveSettings() {
+    var language = document.getElementById('language').value;
+    var deeplKey = encrypt(document.getElementById('deepl-key').value);
+    var labs11Key = encrypt(document.getElementById('11labs-key').value);
+
+    var settings = {
+        language: language,
+        deeplKey: deeplKey,
+        labs11Key: labs11Key
+    };
+
+    var settingsStr = JSON.stringify(settings);
+
+    var script = "try {" +
+                 "    var folder = new Folder('~/Documents/ppCaption2Audio');" +
+                 "    if (!folder.exists) {" +
+                 "        folder.create();" +
+                 "    }" +
+                 "    var file = new File(folder.fsName + '/settings.txt');" +
+                 "    file.open('w');" +
+                 "    file.write('" + settingsStr + "');" +
+                 "    file.close();" +
+                 "    'Settings saved successfully.';" +
+                 "} catch (error) {" +
+                 "    'Error: ' + error.toString();" +
+                 "}";
+
+    csInterface.evalScript(script, function(result) {
+        console.log(result); // Log the result
+    });
+}
+
+window.onload = function() {
+    var script = "try {" +
+                 "    var folder = new Folder('~/Documents/ppCaption2Audio');" +
+                 "    if (!folder.exists) {" +
+                 "        folder.create();" +
+                 "    }" +
+                 "    var file = new File(folder.fsName + '/settings.txt');" +
+                 "    file.open('r');" +
+                 "    var settingsStr = file.read();" +
+                 "    file.close();" +
+                 "    settingsStr;" +
+                 "} catch (error) {" +
+                 "    'Error: ' + error.toString();" +
+                 "}";
+
+    csInterface.evalScript(script, function(result) {
+        if (result.startsWith('Error:')) {
+            console.log(result); // Log the error
+        } else {
+            var settings = JSON.parse(result);
+
+            if (settings.language) {
+                document.getElementById('language').value = settings.language;
+            }
+            if (settings.deeplKey) {
+                document.getElementById('deepl-key').value = decrypt(settings.deeplKey);
+            }
+            if (settings.labs11Key) {
+                document.getElementById('11labs-key').value = decrypt(settings.labs11Key);
+            }
+        }
+    });
+};
